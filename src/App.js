6ed52321a2c931eeb467c9991fd776c7bcff3080
Raymond Lehlohonolo Mashigo <thlogi712@gmail.com>
2021-05-08 13:05:59 +0200

    fetching coordinates from the api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import Map from './components/map';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [longitude, setLongitude] = useState(-12.0691)
+  const [latitude, setLatitude] = useState(-47.2433)
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+
+  const getLocation = async () => {
+    setLoading(true)
+    const res = await axios.get('http://api.open-notify.org/iss-now.json')
+    const { longitude, latitude } = await res.data.iss_position
+
+    setLatitude(parseFloat(latitude))
+    setLongitude(parseFloat(longitude))
+    setLoading(false)
+  }
   return (
     <div className="App">
-      Hello world!!
+      <h3>ISS Tracker</h3>
+      {!loading ? (
+
+        <Map center={{lat: latitude, lng: longitude}} />
+      ) : (
+        <h1>loading</h1>
+      )
+
+      }
     </div>
   );
 }
